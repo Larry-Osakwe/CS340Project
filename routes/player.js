231189c1030a1a74685cd4ab26125dcaee3a5e4e
Larry Osakwe <osakwel@flip2.engr.oregonstate.edu>
2019-03-13 21:54:42 -0700

    fix add and delete

--- routes/player.js
+++ routes/player.js
@@ -18,10 +18,6 @@ module.exports = {
         let position = req.body.position;
         let number = req.body.number;
         let username = req.body.username;
-        let uploadedFile = req.files.image;
-        let image_name = uploadedFile.name;
-        let fileExtension = uploadedFile.mimetype.split('/')[1];
-        image_name = username + '.' + fileExtension;
 
         let usernameQuery = "SELECT * FROM `players` WHERE user_name = '" + username + "'";
 
@@ -37,29 +33,19 @@ module.exports = {
                 });
             } else {
                 // check the filetype before uploading it
-                if (uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif') {
-                    // upload the file to the /public/assets/img directory
-                    uploadedFile.mv(`public/assets/img/${image_name}`, (err ) => {
-                        if (err) {
-                            return res.status(500).send(err);
-                        }
-                        // send the player's details to the database
-                        let query = "INSERT INTO `players` (first_name, last_name, position, number, image, user_name) VALUES ('" +
-                            first_name + "', '" + last_name + "', '" + position + "', '" + number + "', '" + image_name + "', '" + username + "')";
-                        db.query(query, (err, result) => {
-                            if (err) {
-                                return res.status(500).send(err);
-                            }
-                            res.redirect('/');
-                        });
-                    });
-                } else {
-                    message = "Invalid File format. Only 'gif', 'jpeg' and 'png' images are allowed.";
-                    res.render('add-player.ejs', {
-                        message,
-                        title: 'Welcome to Socka | Add a new player'
-                    });
-                }
+                
+                    
+                // send the player's details to the database
+                let query = "INSERT INTO `players` (first_name, last_name, position, number, image, user_name) VALUES ('" +
+                    first_name + "', '" + last_name + "', '" + position + "', '" + number + "', '" + "NULL" + "', '" + username + "')";
+                db.query(query, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/');
+                });
+                    
+                
             }
         });
     },
@@ -94,27 +80,13 @@ module.exports = {
     },
     deletePlayer: (req, res) => {
         let playerId = req.params.id;
-        let getImageQuery = 'SELECT image from `players` WHERE id = "' + playerId + '"';
         let deleteUserQuery = 'DELETE FROM players WHERE id = "' + playerId + '"';
 
-        db.query(getImageQuery, (err, result) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-
-            let image = result[0].image;
-
-            fs.unlink(`public/assets/img/${image}`, (err) => {
-                if (err) {
-                    return res.status(500).send(err);
-                }
-                db.query(deleteUserQuery, (err, result) => {
+        db.query(deleteUserQuery, (err, result) => {
                     if (err) {
                         return res.status(500).send(err);
                     }
                     res.redirect('/');
                 });
-            });
-        });
     }
 };
